9c832d851cb4cf4327aa9105b9bb59993543af99
zhoujun <572459439@qq.com>
2019-12-21 20:59:21 +0800

    fix resume bug

---
+++ models/modules/seg/resnet_fpn.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: zhoujun
+@time: 2019/12/17 上午11:02
+'''
+import torch
+from torch import nn
+import torch.nn.functional as F
+from models.modules.basic import ConvBnRelu
+
+from models.modules.seg.resnet import *
+
+
+class FPN(nn.Module):
+    def __init__(self, backbone_out_channels, inner_channels=256):
+        """
+        :param backbone_out_channels: 基础网络输出的维度
+        :param kwargs:
+        """
+        super().__init__()
+        inplace = True
+        self.conv_out_channels = inner_channels
+        inner_channels = inner_channels // 4
+        # reduce layers
+        self.reduce_conv_c2 = ConvBnRelu(backbone_out_channels[0], inner_channels, kernel_size=1, inplace=inplace)
+        self.reduce_conv_c3 = ConvBnRelu(backbone_out_channels[1], inner_channels, kernel_size=1, inplace=inplace)
+        self.reduce_conv_c4 = ConvBnRelu(backbone_out_channels[2], inner_channels, kernel_size=1, inplace=inplace)
+        self.reduce_conv_c5 = ConvBnRelu(backbone_out_channels[3], inner_channels, kernel_size=1, inplace=inplace)
+        # Smooth layers
+        self.smooth_p4 = ConvBnRelu(inner_channels, inner_channels, kernel_size=3, padding=1, inplace=inplace)
+        self.smooth_p3 = ConvBnRelu(inner_channels, inner_channels, kernel_size=3, padding=1, inplace=inplace)
+        self.smooth_p2 = ConvBnRelu(inner_channels, inner_channels, kernel_size=3, padding=1, inplace=inplace)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.conv_out_channels, self.conv_out_channels, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(self.conv_out_channels),
+            nn.ReLU(inplace=inplace)
+        )
+        self.out_conv = nn.Sequential(
+            nn.Conv2d(in_channels=self.conv_out_channels, out_channels=1, kernel_size=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        c2, c3, c4, c5 = x
+        # Top-down
+        p5 = self.reduce_conv_c5(c5)
+        p4 = self._upsample_add(p5, self.reduce_conv_c4(c4))
+        p4 = self.smooth_p4(p4)
+        p3 = self._upsample_add(p4, self.reduce_conv_c3(c3))
+        p3 = self.smooth_p3(p3)
+        p2 = self._upsample_add(p3, self.reduce_conv_c2(c2))
+        p2 = self.smooth_p2(p2)
+
+        x = self._upsample_cat(p2, p3, p4, p5)
+        x = self.conv(x)
+        x = self.out_conv(x)
+        return x
+
+    def _upsample_add(self, x, y):
+        return F.interpolate(x, size=y.size()[2:]) + y
+
+    def _upsample_cat(self, p2, p3, p4, p5):
+        h, w = p2.size()[2:]
+        p3 = F.interpolate(p3, size=(h, w))
+        p4 = F.interpolate(p4, size=(h, w))
+        p5 = F.interpolate(p5, size=(h, w))
+        return torch.cat([p2, p3, p4, p5], dim=1)
+
+
+class ResNetFPN(nn.Module):
+    def __init__(self, backbone, pretrained, **kwargs):
+        """
+        PANnet
+        :param model_config: 模型配置
+        """
+        super().__init__()
+
+        backbone_dict = {
+            'resnet18': {'models': resnet18, 'out': [64, 128, 256, 512]},
+            'deformable_resnet18': {'models': deformable_resnet18, 'out': [64, 128, 256, 512]},
+            'resnet34': {'models': resnet34, 'out': [64, 128, 256, 512]},
+            'resnet50': {'models': resnet50, 'out': [256, 512, 1024, 2048]},
+            'deformable_resnet50': {'models': deformable_resnet50, 'out': [256, 512, 1024, 2048]},
+            'resnet101': {'models': resnet101, 'out': [256, 512, 1024, 2048]},
+            'resnet152': {'models': resnet152, 'out': [256, 512, 1024, 2048]},
+        }
+        assert backbone in backbone_dict, 'backbone must in: {}'.format(backbone_dict)
+        backbone_model, backbone_out = backbone_dict[backbone]['models'], backbone_dict[backbone]['out']
+        self.backbone = backbone_model(pretrained=pretrained)
+        self.segmentation_head = FPN(backbone_out)
+        self.out_channels = 1
+
+    def forward(self, x):
+        _, _, H, W = x.size()
+        backbone_out = self.backbone(x)
+        y = self.segmentation_head(backbone_out)
+        y = F.interpolate(y, size=(H, W))
+        return y
---
+++ models/modules/seg/unet.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: zhoujun
+@time: 2019/12/17 下午1:51
+'''
+
+from torch import nn
+import torch.nn.functional as F
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.1)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class DownBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            ConvBlock(in_channels, out_channels, 3),
+            ConvBlock(out_channels, out_channels, 3)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class UpBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, shrink=True, **kwargs):
+        super().__init__()
+        self.conv3_0 = ConvBlock(in_channels, out_channels, 3)
+        if shrink:
+            self.conv3_1 = ConvBlock(out_channels, int(out_channels / 2), 3)
+        else:
+            self.conv3_1 = ConvBlock(out_channels, out_channels, 3)
+
+    def forward(self, x, s):
+        x = F.interpolate(x, scale_factor=2)
+
+        x = torch.cat([s, x], dim=1)
+        x = self.conv3_0(x)
+        x = self.conv3_1(x)
+        return x
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, **kwargs):
+        super().__init__()
+        self.stage_channels = [32, 64, 128, 256, 512]
+        self.d0 = DownBlock(in_channels, self.stage_channels[0])
+
+        self.d1 = nn.Sequential(nn.MaxPool2d(2, 2, ceil_mode=True), DownBlock(self.stage_channels[0], self.stage_channels[1]))
+
+        self.d2 = nn.Sequential(nn.MaxPool2d(2, 2, ceil_mode=True), DownBlock(self.stage_channels[1], self.stage_channels[2]))
+
+        self.d3 = nn.Sequential(nn.MaxPool2d(2, 2, ceil_mode=True), DownBlock(self.stage_channels[2], self.stage_channels[3]))
+
+        self.d4 = nn.Sequential(nn.MaxPool2d(2, 2, ceil_mode=True), DownBlock(self.stage_channels[3], self.stage_channels[4]))
+
+        self.u3 = UpBlock(self.stage_channels[3] + self.stage_channels[4], self.stage_channels[3], shrink=True)
+        self.u2 = UpBlock(self.stage_channels[3], self.stage_channels[2], shrink=True)
+        self.u1 = UpBlock(self.stage_channels[2], self.stage_channels[1], shrink=True)
+        self.u0 = UpBlock(self.stage_channels[1], self.stage_channels[0], shrink=False)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(self.stage_channels[0], 1, 1, bias=False),
+            nn.Sigmoid()
+        )
+        self.out_channels = 1
+
+    def forward(self, x):
+        x0 = self.d0(x)
+        x1 = self.d1(x0)
+        x2 = self.d2(x1)
+        x3 = self.d3(x2)
+        x4 = self.d4(x3)
+
+        y3 = self.u3(x4, x3)
+        y2 = self.u2(y3, x2)
+        y1 = self.u1(y2, x1)
+        y0 = self.u0(y1, x0)
+        out = self.conv(y0)
+        return out
+
+
+if __name__ == '__main__':
+    import torch
+
+    x = torch.zeros(1, 3, 32, 320)
+    net = UNet(3)
+    y = net(x)
+    print(y.shape)
